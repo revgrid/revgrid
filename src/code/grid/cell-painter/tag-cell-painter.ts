8b87518effc89baf8b69870a27968af26628a8b7
Paul Klink <paul@klink.au>
2023-03-28 10:20:28 +1100

    Upgrade Typescript to 5

--- src/code/grid/cell-painter/tag-cell-painter.ts
+++ src/code/grid/cell-painter/tag-cell-painter.ts
@@ -6,28 +6,28 @@ import { CellPainter } from './cell-painter';
 export class TagCellPainter extends CellPainter {
 
     override paint(gc: CanvasRenderingContext2DEx, config: CellPaintConfig) {
-        const tagbands: TagCellPainter.Tagband[] | undefined = undefined; // should be config.tagbands
-        if (tagbands) {
-            const tagband = tagbands.find((tagband) => {
-                return config.value >= tagband.floor;
-            });
-            const fillStyle = tagband && tagband.fillStyle;
+        // const tagbands: TagCellPainter.Tagband[] | undefined = undefined; // should be config.tagbands
+        // if (tagbands) {
+        //     const tagband = tagbands.find((tagband) => {
+        //         return config.value >= tagband.floor;
+        //     });
+        //     const fillStyle = tagband && tagband.fillStyle;
 
-            if (fillStyle) {
-                const b = config.bounds,
-                    x = b.x + b.width - 1,
-                    y = b.y;
+        //     if (fillStyle) {
+        //         const b = config.bounds,
+        //             x = b.x + b.width - 1,
+        //             y = b.y;
 
-                gc.beginPath();
-                gc.moveTo(x, y);
-                gc.lineTo(x, y + 8);
-                gc.lineTo(x - 8, y);
-                // gc.lineTo(x, y);
-                gc.closePath();
-                gc.cache.fillStyle = fillStyle;
-                gc.fill();
-            }
-        }
+        //         gc.beginPath();
+        //         gc.moveTo(x, y);
+        //         gc.lineTo(x, y + 8);
+        //         gc.lineTo(x - 8, y);
+        //         // gc.lineTo(x, y);
+        //         gc.closePath();
+        //         gc.cache.fillStyle = fillStyle;
+        //         gc.fill();
+        //     }
+        // }
     }
 }
 
