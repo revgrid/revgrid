0c78588cae7c3b5ed4a6c2963b2e24138b24b33f
Paul Klink <paul@klink.au>
2024-03-29 07:17:08 +1100

    Remove record Server Set

--- src/code/server-sets/record/server-sets-record-public-api.ts
+++
@@ -1,9 +0,0 @@
-export { RevRecord, RevRecordData } from './rev-record';
-export { RevRecordDataServer } from './rev-record-data-server';
-export * from './rev-record-error';
-export { RevRecordField } from './rev-record-field';
-export * from './rev-record-functionize-field';
-export { RevRecordSchemaServer } from './rev-record-schema-server';
-export { RevRecordDataStore, RevRecordStore } from './rev-record-store';
-export * from './rev-record-types';
-
--- src/code/server-sets/server-sets-public-api.ts
+++ src/code/server-sets/server-sets-public-api.ts
@@ -1,5 +1,4 @@
 export * from './data-row-array/server-sets-data-row-array-public-api';
 export * from './multi-heading/server-sets-multi-heading-public-api';
-export * from './record/server-sets-record-public-api';
 export * from './single-heading/server-sets-single-heading-public-api';
 
